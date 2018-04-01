0cc3fc5f11965c8f4435f058463db5edfeba0998
Peter Tieu <tieu.peter.v@gmail.com>
2018-04-01 22:23:37 +1000

    Added directory Sound asset file directories for languages: Thai and Vietnamese

--- app/src/main/java/com/petertieu/android/localbox/CategoryFragment.java
+++ app/src/main/java/com/petertieu/android/localbox/CategoryFragment.java
@@ -2,6 +2,7 @@ package com.petertieu.android.localbox;
 
 
 import android.databinding.DataBindingUtil;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -10,41 +11,44 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 import com.petertieu.android.localbox.databinding.FragmentLocalBoxBinding;
 import com.petertieu.android.localbox.databinding.ListItemSoundBinding;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 
-/**
- * Created by Peter Tieu on 19/03/2018.
- */
 
 public class CategoryFragment extends Fragment {
 
     //============= Declare instance variables ==============================================
+
     private final String TAG = "CategoryFragment";
-    private String mLanguageChosen;
-    private String mCategoryChosen;
+    public static String mLanguageChosen;
+    public static String mCategoryChosen;
 
 
     private SoundManager mSoundManager;
 
 
 
+    //============= Declare methods =========================================================
+
     //Override onCreate(..) fragment lifecycle callback method
     @Override
     public void onCreate(Bundle onSaveInstanceState){
         super.onCreate(onSaveInstanceState);
 
+        //Log to Logcat
         Log.i(TAG, "onCreate(..) called");
 
 
+        //Get the l
         if (getArguments() != null){
-
 
             mLanguageChosen = getArguments().getString(CategoryActivity.KEY_LANGUAGE_CHOSEN);
 
@@ -56,7 +60,140 @@ public class CategoryFragment extends Fragment {
 
 
 
+
+
+
+        //TODO: Put all of the below in a method!
+        switch (mLanguageChosen){
+
+            case ("chinese"):
+                switch(mCategoryChosen){
+
+                    case("numerics"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/chinese/numerics";
+                        break;
+
+                    case("statements"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/chinese/statements";
+                        break;
+
+                    case("questions"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/chinese/questions";
+                        break;
+
+
+                    case("adjectives"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/chinese/adjectives";
+                        break;
+
+                    case("nouns"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/chinese/nouns";
+                        break;
+
+                    default:
+                        SoundManager.SOUNDS_FOLDER_NAME = "";
+                        break;
+                }
+
+                break;
+
+
+
+
+
+            case("vietnamese"):
+                switch(mCategoryChosen){
+
+                    case("numerics"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/vietnamese/numerics";
+                        break;
+
+
+                    case("statements"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/vietnamese/statements";
+                        break;
+
+                    case("questions"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/vietnamese/questions";
+                        break;
+
+
+                    case("adjectives"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/vietnamese/adjectives";
+                        break;
+
+                    case("nouns"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/vietnamese/nouns";
+                        break;
+
+                    default:
+                        SoundManager.SOUNDS_FOLDER_NAME = "";
+                        break;
+                }
+
+                break;
+
+
+
+
+
+
+
+
+            case("thai"):
+                switch(mCategoryChosen){
+
+                    case("numerics"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/thai/numerics";
+                        break;
+
+
+                    case("statements"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/thai/statements";
+                        break;
+
+                    case("questions"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/thai/questions";
+                        break;
+
+
+                    case("adjectives"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/thai/adjectives";
+                        break;
+
+                    case("nouns"):
+                        SoundManager.SOUNDS_FOLDER_NAME = "all_sounds/thai/nouns";
+                        break;
+
+                    default:
+                        SoundManager.SOUNDS_FOLDER_NAME = "";
+                        break;
+                }
+
+                break;
+
+
+
+
+
+
+
+
+
+
+
+            default:
+                SoundManager.SOUNDS_FOLDER_NAME = "";
+                break;
+
+
+
+        }
+
+
+
         mSoundManager = new SoundManager(getActivity());
+
 
 
         setRetainInstance(true);
@@ -66,10 +203,13 @@ public class CategoryFragment extends Fragment {
 
 
 
+    Button mButton;
+
+
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle savedIstanceState){
 
-        FragmentLocalBoxBinding fragmentLocalboxBinding;
+//        FragmentLocalBoxBinding fragmentLocalboxBinding;
 
         fragmentLocalboxBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_local_box, viewGroup, false);
 
@@ -79,11 +219,18 @@ public class CategoryFragment extends Fragment {
         fragmentLocalboxBinding.recyclerView.setAdapter(new SoundAdapter(mSoundManager.getSounds()));
 
 
+
+
+        //Equivalent to returning "view" - returns the outermost View in the layout file associated with the Binding
         return fragmentLocalboxBinding.getRoot();
+
+
 
 
     }
 
+
+    public static FragmentLocalBoxBinding fragmentLocalboxBinding;
 
 
 
@@ -137,6 +284,11 @@ public class CategoryFragment extends Fragment {
             Sound sound = mSounds.get(position);
 
             soundHolder.bind(sound);
+
+
+
+
+
         }
 
 
@@ -159,44 +311,65 @@ public class CategoryFragment extends Fragment {
 
 
 
-    private class SoundHolder extends RecyclerView.ViewHolder{
 
 
-        ListItemSoundBinding mListItemSoundBinding;
+
+//    public static ListItemSoundBinding mListItemSoundBinding;
+
+
+    public class SoundHolder extends RecyclerView.ViewHolder{
+
+
+        public ListItemSoundBinding mListItemSoundBinding;
 
 
         private SoundHolder(ListItemSoundBinding listItemSoundBinding){
 
             super(listItemSoundBinding.getRoot());
 
+
             mListItemSoundBinding = listItemSoundBinding;
 
 
+            //Set the variable of the list_item_sound to be the ViewModel (SoundViewModel)
+            mListItemSoundBinding.setSoundViewModel(new SoundViewModel(mSoundManager, listItemSoundBinding.getRoot().getContext()));
 
-            mListItemSoundBinding.setSoundViewModel(new SoundViewModel(mSoundManager));
+
+
+
+
         }
 
 
 
-        public void bind(Sound sound){
+
+
+
+        public void bind(final Sound sound){
 
 
 
             mListItemSoundBinding.getSoundViewModel().setSound(sound);
 
+
+            //Can be omitted: "Evaluates the pending bindings, updating any Views that have expressions bound to modified variables"
             mListItemSoundBinding.executePendingBindings();
+
+
+
+
+
+//            mSound = sound;
+
 
         }
 
 
 
 
+
+
     }
-
-
-
-
-
 
 
 
