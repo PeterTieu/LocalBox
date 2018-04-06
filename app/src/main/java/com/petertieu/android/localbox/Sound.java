0b1be83f79ee98f415fee1a91e639dc7bb4b4658
Peter Tieu <tieu.peter.v@gmail.com>
2018-04-06 23:54:03 +1000

    Removed the ".mp3" component from the Sound pathname and separated parts of the Sound pathname by underscores

--- app/src/main/java/com/petertieu/android/localbox/Sound.java
+++ app/src/main/java/com/petertieu/android/localbox/Sound.java
@@ -6,17 +6,34 @@ package com.petertieu.android.localbox;
 
 //In MODEL layer of the project
 
+import android.media.MediaMetadataRetriever;
+import android.util.Log;
+
+import java.io.IOException;
+
 public class Sound {
 
     //============= Declare instance variables ==============================================
+
+    private final String TAG = "Sound";
+
     //The filepath of the Sound asset file
     private String mSoundFilePath;
+
+    //ID of the of the Sound asset file - of type Integer, as it does not carry a primative value
+    private Integer mSoundId;
 
     //Name of the Sound asset file (i.e. after the directory components have been stripped)
     private String mSoundName;
 
-    //ID of the of the Sound asset file - of type Integer, as it does not carry a primative value
-    private Integer mSoundId;
+    //Sound name - English translation
+    private String mSoundNameEnglish;
+
+    //Sound name - Native language translation
+    private String mSoundNameLanguage;
+
+    private String mSoundPronounciation;
+
 
 
 
@@ -34,8 +51,39 @@ public class Sound {
         //Obtain the FILENAME of the sound (i.e. the last component of the sound filepath, e.g. "mi_nombre_es.mp3");
         String soundFilename = componentsOfSoundFilePath[componentsOfSoundFilePath.length - 1];
 
-        //Get the RAW name of the Sound. Replace the ".mp3" part of the Sound's name to a blank.
-        mSoundName = soundFilename.replace(".mp3", "");
+        //Remove the ".mp3" component from the Sound filename
+        String soundFilenameWithoutFiletype = soundFilename.replace(".mp3", "");
+
+
+        String[] componentsOfSoundFileName = soundFilenameWithoutFiletype.split("_");
+
+        mSoundNameEnglish = componentsOfSoundFileName[1];
+
+        mSoundNameLanguage = componentsOfSoundFileName[2];
+
+        if (componentsOfSoundFileName[3] != null) {
+            mSoundPronounciation = componentsOfSoundFileName[3];
+        }
+
+
+
+        Log.i(TAG, "Sound filename English: " + mSoundNameEnglish);
+        Log.i(TAG, "Sound filename Language: " + mSoundNameLanguage);
+
+        if (mSoundPronounciation != null) {
+            Log.i(TAG, "Sound phonetics: " + mSoundPronounciation);
+        }
+
+
+
+
+        //Get the RAW name of the Sound. Remove the following components: ".mp3", numeric, and the underscore ("_") between the numeric and the name
+        mSoundName = soundFilename.replace(".mp3", "").replaceAll("\\d", "").replace("_", "");
+
+
+
+
+
     }
 
 
@@ -46,6 +94,16 @@ public class Sound {
     }
 
 
+    //Get the ID of the Sound
+    public Integer getSoundId(){
+        return mSoundId;
+    }
+
+
+    //Set the ID of the Sound
+    public void setSoundId(Integer soundId){
+        mSoundId = soundId;
+    }
 
     //Get the name of the Sound
     public String getSoundName(){
@@ -53,16 +111,33 @@ public class Sound {
     }
 
 
+//    //Get the title of the Sound
+//    public String getTitle(){
+//        return mSoundTitle;
+//    }
 
-    //Get the ID of the Sound
-    public Integer getSoundId(){
-        return mSoundId;
+
+    public String getSoundNameEnglish(){
+        return mSoundNameEnglish;
+    }
+
+
+    public String getSoundNameLanguage(){
+        return mSoundNameLanguage;
+    }
+
+
+    public String getSoundPronounciation(){
+        return mSoundPronounciation;
     }
 
 
 
-    //Set the ID of the Sound
-    public void setSoundId(Integer soundId){
-        mSoundId = soundId;
-    }
+
+
+
+
+
+
+
 }
