8b583f1e26bcfd3d0943ff2167dfbd84c55b7996
Peter Tieu <tieu.peter.v@gmail.com>
2018-04-20 09:10:47 +1000

    Added a customised action bar for each language for the CategoryActivity (i.e. activity hosting CategoryFragment)

--- app/src/main/java/com/petertieu/android/localbox/CategoryActivity.java
+++ app/src/main/java/com/petertieu/android/localbox/CategoryActivity.java
@@ -4,7 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Peter Tieu on 19/03/2018.
@@ -60,8 +65,105 @@ public class CategoryActivity extends OneFragmentActivity{
         String languageChosenCapitalised = mLanguageChosen.substring(0,1).toUpperCase() + mLanguageChosen.substring(1);
 
         String categoryChosenCapitalised = mCategoryChosen.substring(0,1).toUpperCase() + mCategoryChosen.substring(1);
+//
+//        getSupportActionBar().setTitle(languageChosenCapitalised + " " + categoryChosenCapitalised);
 
-        getSupportActionBar().setTitle(languageChosenCapitalised + " " + categoryChosenCapitalised);
+
+
+
+
+
+
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+
+
+
+
+        LayoutInflater layoutInflator = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = layoutInflator.inflate(R.layout.action_bar_category_activity, null);
+        TextView textView = (TextView) v.findViewById(R.id.text_view);
+
+        textView.setText(languageChosenCapitalised + " " + categoryChosenCapitalised);
+
+
+
+
+
+
+
+        ImageView flagActionBarImageView = (ImageView) v.findViewById(R.id.flag_action_bar);
+
+        int flagActionBarResourceId = 0;
+
+
+        //TODO: Make a method out of this////////////////////////////////////////////////////////////////////////////////////
+        switch (mLanguageChosen){
+            case ("arabic"):
+                flagActionBarResourceId = R.drawable.arabic_flag;
+                break;
+
+            case ("chinese"):
+                flagActionBarResourceId = R.drawable.chinese_flag;
+                break;
+
+
+            case ("french"):
+                flagActionBarResourceId = R.drawable.french_flag;
+                break;
+
+
+            case ("german"):
+                flagActionBarResourceId = R.drawable.german_flag;
+                break;
+
+            case ("hindi"):
+                flagActionBarResourceId = R.drawable.hindi_flag;
+                break;
+
+            case ("italian"):
+                flagActionBarResourceId = R.drawable.italian_flag;
+                break;
+
+            case ("japanese"):
+                flagActionBarResourceId = R.drawable.japanese_flag;
+                break;
+
+            case ("korean"):
+                flagActionBarResourceId = R.drawable.korean_flag;
+                break;
+
+            case ("russian"):
+                flagActionBarResourceId = R.drawable.russian_flag;
+                break;
+
+            case ("spanish"):
+                flagActionBarResourceId = R.drawable.spanish_flag;
+                break;
+
+            case ("thai"):
+                flagActionBarResourceId = R.drawable.thai_flag;
+                break;
+
+            case ("vietnamese"):
+                flagActionBarResourceId = R.drawable.vietnamese_flag;
+                break;
+        }
+
+
+        flagActionBarImageView.setImageDrawable(getResources().getDrawable(flagActionBarResourceId));
+
+
+
+
+
+
+
+
+
+        actionBar.setCustomView(v);
 
 
     }
